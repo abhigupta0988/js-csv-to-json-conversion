486bcea0f7ba6acca10bfb04c667a6a1b51487d3
Abhishek Gupta <abhishek.gupta_cs13@gla.ac.in>
2017-08-18 12:38:49 +0530

    update and added
    updated code of urban & rural population % age
    urban growth %age is added

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,7 +1,9 @@
 const readline = require('readline');
 const fs = require('fs');
-let year_object = {};
-let write_to_json = fs.createWriteStream('urban.json');
+var year_object = {};
+var urb_pop_grow = {};
+var write_to_json_urban = fs.createWriteStream('urb-rur-total-pop.json');
+var write_to_json_urb_pop = fs.createWriteStream('urb-pop-growth.json');
 const rl = readline.createInterface({
   input: fs.createReadStream('indicators.csv')
 });
@@ -11,29 +13,27 @@ rl.on('line', (line) => {
   .split('\n')
   var arr = line.split(',')
   if(arr[1] === 'IND' && arr[3] === 'SP.URB.TOTL.IN.ZS'){
-		 let temp = [
-    						{ 'year': arr[4] },
-    						{ 'value': arr[5] }
-			];
-			year_object[temp[0]] = year_object[temp[0]] || [];
-				year_object[temp[1]].push({
-					"Urban total Population " : temp[1]
-					}) 
-
- 			
+			year_object[arr[4]] = year_object[arr[4]] || [];	
+					year_object[arr[4]].push({
+					"Urban Population (% in total)" : arr[5]
+					});
+  }
+ 	if(arr[1] === 'IND' && arr[3] === 'SP.RUR.TOTL.ZS'){
+			year_object[arr[4]] = year_object[arr[4]] || [];		
+					year_object[arr[4]].push({
+					"Rural Population (% in total)" : arr[5]
+					});
 
   }
-  rl.on('end',function() {demo
-	write_to_json.write(JSON.stringify(year_object,null,2),'UTF-8');
-});
+  if(arr[1] === 'IND' && arr[3] === 'SP.URB.GROW'){
+			urb_pop_grow[arr[4]] = urb_pop_grow[arr[4]] || [];	
+					urb_pop_grow[arr[4]].push({
+					"Urban population growth(annual %)" : arr[5]
+					});
+			
 
-
-
-
-
-
-
-
-  	
-
+  }
+}).on('close', () => {
+  write_to_json_urban.write(JSON.stringify(year_object,null,2),'UTF-8');
+  write_to_json_urb_pop.write(JSON.stringify(urb_pop_grow,null,2),'UTF-8');
 });

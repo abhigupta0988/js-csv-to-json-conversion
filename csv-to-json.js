de8f3334d795624b6dd7ad1c2deb8390a2d6263b
Abhishek Gupta <abhishek.gupta_cs13@gla.ac.in>
2017-08-23 12:15:47 +0530

    Add files via upload
    update structure of json

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); //import 'fs' or file system module of nodejs
 const readline = require('readline');  //import 'readline' module of nodejs
-let urb_pop_grow = {}, urb_rur_population = {}, year_object = {};
+let urb_pop_grow = [], urb_rur_population = [], year_object = [];
 let count = 1,index = [];
 let asia = ['ARM','AZE','BHR','BGD','BTN','BRN','KHM','CHN','CXR','CCK','IOT','GEO','HKG','IND','IDN','IRN','IRQ','ISR','JPN','JOR','KAZ','KWT','KGZ','LAO','LBN','MAC','MYS','MDV','MNG','MMR','NPL','PRK','OMN','PAK','PSE','PHL','QAT','SAU','SGP','KOR','LKA','SYR','TWN','TJK','THA','TUR','TKM','ARE','UZB','VNM','YEM'];
 
@@ -20,36 +20,37 @@ rl.on('line', (line) => {
 		index['CountryCode'] = line_holder.indexOf('CountryCode');
 		count--;
 	}
-	if(line_holder[1] === 'IND' && line_holder[index['IndicatorCode']] === 'SP.URB.TOTL.IN.ZS'){	//comparing for urban population
-		year_object[line_holder[index['Year']]] = year_object[line_holder[index['Year']]] || [];	//setting year as object
-		year_object[line_holder[index['Year']]].push({
-			"Urban Population (% in total)" : parseFloat(line_holder[index['Value']])
+	if(line_holder[index['CountryCode']] == "IND" && (line_holder[index['IndicatorCode']] == "SP.URB.TOTL.IN.ZS" || line_holder[index['IndicatorCode']] == "SP.RUR.TOTL.ZS")) {
+		if(year_object.find(x => x.year === line_holder[index['Year']]) == undefined) {
+				let temp = { year : line_holder[index['Year']] }
+				temp[line_holder[index['IndicatorCode']] == "SP.URB.TOTL.IN.ZS"?'urban':'rural'] = parseFloat(line_holder[index['Value']]);
+				year_object.push(temp);
+			}
+			else {
+				let index_of_year = year_object.findIndex(x => x.year === line_holder[index['Year']]);
+				year_object[index_of_year][line_holder[index['IndicatorCode']] == "SP.URB.TOTL.IN.ZS"?'urban':'rural'] = parseFloat(line_holder[index['Value']]);
+			}
+		}
+	if(line_holder[index['CountryCode']] === 'IND' && line_holder[index['IndicatorCode']] === 'SP.URB.GROW'){	//comparing for urban population growth
+		urb_pop_grow.push({
+			"Year" : parseInt(line_holder[index['Year']]),
+			"urban_growth" : parseFloat(line_holder[index['Value']])
 		});
 	}
-	if(line_holder[1] === 'IND' && line_holder[index['IndicatorCode']] === 'SP.RUR.TOTL.ZS'){	//comparing for rural population
-		year_object[line_holder[index['Year']]] = year_object[line_holder[index['Year']]] || [];	//setting year as object
-		year_object[line_holder[index['Year']]].push({
-			"Rural Population (% in total)" : parseFloat(line_holder[index['Value']])
-		});
-	}
-	if(line_holder[1] === 'IND' && line_holder[index['IndicatorCode']] === 'SP.URB.GROW'){	//comparing for urban population growth
-		urb_pop_grow[line_holder[index['Year']]] = urb_pop_grow[line_holder[index['Year']]] || [];	//setting year as object
-		urb_pop_grow[line_holder[index['Year']]].push({
-			"Urban population growth(annual %)" : parseFloat(line_holder[index['Value']])
-		});
-	}
-	if((asia.find(x => x === line_holder[1]) != undefined)){	//comparing for countries in asia and total population
+	if((asia.find(x => x === line_holder[index['CountryCode']]) != undefined)){	//comparing for countries in asia and total population
 		if(line_holder[index['IndicatorCode']] === 'SP.POP.TOTL'){
-			urb_rur_population[line_holder[index['Year']]] 
-				= urb_rur_population[line_holder[index['Year']]] || [];	//setting year as object
-			urb_rur_population[line_holder[index['Year']]].push({
-				"country name" : line_holder[index['CountryName']].replace(/\"/g , ""), //replace "abc" to abc
-				"Urban Population + Rural Population" : parseInt(line_holder[index['Value']])
+			urb_rur_population.push({
+				"Year" : parseInt(line_holder[index['Year']]),
+				"Country_Code" : line_holder[index['CountryCode']],
+				"Total_Population" : parseFloat(line_holder[index['Value']]),
 			});
 		}
 	}
 })
 .on('close', () => {
+	let x=1;
+    let y=[];
+    urb_rur_population.sort((a,b)=>{return b.Total_Population - a.Total_Population});
 	fs.createWriteStream('urb-rur-total-pop.json').write(JSON.stringify(year_object,null,2)); //writing year_object to urb-rur-total-pop.json
 	fs.createWriteStream('urb-pop-growth.json').write(JSON.stringify(urb_pop_grow,null,2));	//writing urb_pop_grow to urb-pop-growth.json
 	fs.createWriteStream('asia-total-pop.json').write(JSON.stringify(urb_rur_population,null,2));	//writing urb_rur_population to asia-total-pop.json
